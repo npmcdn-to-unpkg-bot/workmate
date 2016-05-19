0998c6fe03b4ba379de5706bc263f68b206656b2
Stuart George <stuart.bigmassa@gmail.com>
2016-05-19 22:17:50 +0100

    webpack settings changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,48 @@ var webpack = require("webpack");
 
 module.exports = {
     entry: {
-        'agile': './workmate/static/workmate/ng/agile.ts',
-        'contacts': './workmate/static/workmate/ng/contacts.ts',
-        'vendor': './workmate/static/workmate/ng/vendor.ts'
+        'agile'     : './workmate/static/workmate/ng/agile',
+        'contacts'  : './workmate/static/workmate/ng/contacts',
+        'vendor'    : './workmate/static/workmate/ng/vendor'
     },
     output: {
         path: './workmate/static/workmate/dist',
-        filename: '[name].js',
-        pathinfo: true
+        filename: '[name].js'
     },
     resolve: {
         extensions: ['', '.ts', '.js']
     },
     module: {
         loaders: [
-            {test: /\.ts$/, loader: 'ts-loader', exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]}
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: [
+                    /\.(spec|e2e)\.ts$/,
+                    /node_modules\/(?!(ng2-.+))/
+                ]
+            }
         ]
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.js',
+            minChunks: Infinity}),
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({sourceMap: false})
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            beautify: true,
+            mangle: true,
+            unused: true,
+            compress: {
+                screw_ie8: true,
+                keep_fnames: true,
+                drop_debugger: false,
+                dead_code: true,
+                unused: true
+            },
+            comments: false
+        })
     ]
 };
--- workmate/static/workmate/dist/agile.js
+++ workmate/static/workmate/dist/agile.js
@@ -1,31 +1,154 @@
-webpackJsonp([0],{0:/*!**********************************************!*\
-  !*** ./workmate/static/workmate/ng/agile.ts ***!
-  \**********************************************/
-function(t,e,o){"use strict";var n=this&&this.__decorate||function(t,e,o,n){var i,r=arguments.length,s=3>r?e:null===n?n=Object.getOwnPropertyDescriptor(e,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,o,n);else for(var c=t.length-1;c>=0;c--)(i=t[c])&&(s=(3>r?i(s):r>3?i(e,o,s):i(e,o))||s);return r>3&&s&&Object.defineProperty(e,o,s),s},i=this&&this.__metadata||function(t,e){return"object"==typeof Reflect&&"function"==typeof Reflect.metadata?Reflect.metadata(t,e):void 0},r=o(1),s=o(97),c=o(280),a=function(){function t(){}return t=n([r.Component({selector:"agile-app",template:'\n        <div class="ui equal width grid">\n            <div class="column">\n                <div class="ui raised segments" story-list [title]="\'Backlog\'"></div>\n            </div>\n            <div class="column">\n                <div class="ui raised segments" story-list [title]="\'Icebox\'"></div>\n            </div>\n        </div>\n    ',directives:[c.StoryListComponent]}),i("design:paramtypes",[])],t)}();e.AgileComponent=a,s.bootstrap(a)},280:/*!***********************************************************************************!*\
-  !*** ./workmate/static/workmate/ng/components/story-list/story-list.component.ts ***!
-  \***********************************************************************************/
-function(t,e,o){"use strict";var n=this&&this.__decorate||function(t,e,o,n){var i,r=arguments.length,s=3>r?e:null===n?n=Object.getOwnPropertyDescriptor(e,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,o,n);else for(var c=t.length-1;c>=0;c--)(i=t[c])&&(s=(3>r?i(s):r>3?i(e,o,s):i(e,o))||s);return r>3&&s&&Object.defineProperty(e,o,s),s},i=this&&this.__metadata||function(t,e){return"object"==typeof Reflect&&"function"==typeof Reflect.metadata?Reflect.metadata(t,e):void 0},r=o(1),s=o(281),c=o(283),a=o(288),l=function(){function t(t){this.storyService=t,this.newStory=function(){this.stories.push({id:this.stories.length+1,title:"New story"})}}return t.prototype.getStories=function(){var t=this;this.storyService.getStories().then(function(e){return t.stories=e})},t.prototype.ngOnInit=function(){this.getStories()},n([r.Input(),i("design:type",String)],t.prototype,"title",void 0),t=n([r.Component({selector:"[story-list]",template:a.htmlTemplate,directives:[c.StoryListItemComponent],providers:[s.StoryService]}),i("design:paramtypes",[s.StoryService])],t)}();e.StoryListComponent=l},281:/*!***************************************************************!*\
-  !*** ./workmate/static/workmate/ng/services/story.service.ts ***!
-  \***************************************************************/
-function(t,e,o){"use strict";var n=this&&this.__decorate||function(t,e,o,n){var i,r=arguments.length,s=3>r?e:null===n?n=Object.getOwnPropertyDescriptor(e,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,o,n);else for(var c=t.length-1;c>=0;c--)(i=t[c])&&(s=(3>r?i(s):r>3?i(e,o,s):i(e,o))||s);return r>3&&s&&Object.defineProperty(e,o,s),s},i=this&&this.__metadata||function(t,e){return"object"==typeof Reflect&&"function"==typeof Reflect.metadata?Reflect.metadata(t,e):void 0},r=o(1),s=o(282),c=function(){function t(){}return t.prototype.getStories=function(){return Promise.resolve(s.STORIES)},t=n([r.Injectable(),i("design:paramtypes",[])],t)}();e.StoryService=c},282:/*!************************************************************!*\
-  !*** ./workmate/static/workmate/ng/models/mock-stories.ts ***!
-  \************************************************************/
-function(t,e){"use strict";e.STORIES=[{id:1,title:"I want to do foo",effort:.5},{id:2,title:"I want to do bar",effort:2},{id:3,title:"I want to do wiz",effort:1},{id:4,title:"I want to do bang",effort:1},{id:5,title:"I want to do pop",effort:3}]},283:/*!*********************************************************************************************!*\
-  !*** ./workmate/static/workmate/ng/components/story-list-item/story-list-item.component.ts ***!
-  \*********************************************************************************************/
-function(t,e,o){"use strict";var n=this&&this.__decorate||function(t,e,o,n){var i,r=arguments.length,s=3>r?e:null===n?n=Object.getOwnPropertyDescriptor(e,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,o,n);else for(var c=t.length-1;c>=0;c--)(i=t[c])&&(s=(3>r?i(s):r>3?i(e,o,s):i(e,o))||s);return r>3&&s&&Object.defineProperty(e,o,s),s},i=this&&this.__metadata||function(t,e){return"object"==typeof Reflect&&"function"==typeof Reflect.metadata?Reflect.metadata(t,e):void 0},r=o(1),s=o(284),c=o(285),a=o(287),l=function(){function t(){this.open=!1,this.toggle=function(){this.open=!this.open}}return n([r.Input(),i("design:type",s.Story)],t.prototype,"story",void 0),t=n([r.Component({selector:"[story-list-item]",template:a.htmlTemplate,directives:[c.StoryDetailComponent]}),i("design:paramtypes",[])],t)}();e.StoryListItemComponent=l},284:/*!*****************************************************!*\
-  !*** ./workmate/static/workmate/ng/models/story.ts ***!
-  \*****************************************************/
-function(t,e){"use strict";var o=function(){function t(){}return t}();e.Story=o},285:/*!***************************************************************************************!*\
-  !*** ./workmate/static/workmate/ng/components/story-detail/story-detail.component.ts ***!
-  \***************************************************************************************/
-function(t,e,o){"use strict";var n=this&&this.__decorate||function(t,e,o,n){var i,r=arguments.length,s=3>r?e:null===n?n=Object.getOwnPropertyDescriptor(e,o):n;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)s=Reflect.decorate(t,e,o,n);else for(var c=t.length-1;c>=0;c--)(i=t[c])&&(s=(3>r?i(s):r>3?i(e,o,s):i(e,o))||s);return r>3&&s&&Object.defineProperty(e,o,s),s},i=this&&this.__metadata||function(t,e){return"object"==typeof Reflect&&"function"==typeof Reflect.metadata?Reflect.metadata(t,e):void 0},r=o(1),s=o(284),c=o(286),a=function(){function t(){}return n([r.Input(),i("design:type",s.Story)],t.prototype,"story",void 0),t=n([r.Component({selector:"[story-detail]",template:c.htmlTemplate}),i("design:paramtypes",[])],t)}();e.StoryDetailComponent=a},286:/*!********************************************************************************************!*\
-  !*** ./workmate/static/workmate/ng/components/story-detail/story-detail.component.html.ts ***!
-  \********************************************************************************************/
-function(t,e){"use strict";e.htmlTemplate='\n    \n    <div class="ui label">ID<div class="detail">{{ story.id }}</div></div>\n    <div class="ui form">\n        <div class="field">\n            <textarea [(ngModel)]="story.title" rows="2" placeholder="title"></textarea>\n        </div>\n        <div class="field">\n            <select [(ngModel)]="story.effort" class="ui dropdown">\n                <option value="0.5">0.5 Points</option>\n                <option value="1">1 Point</option>\n                <option value="2">2 Points</option>\n                <option value="3">3 Points</option>\n                <option value="5">5 Points</option>\n            </select>\n        </div>\n    </div>\n    \n'},287:/*!**************************************************************************************************!*\
-  !*** ./workmate/static/workmate/ng/components/story-list-item/story-list-item.component.html.ts ***!
-  \**************************************************************************************************/
-function(t,e){"use strict";e.htmlTemplate='\n\n    <div class="item">\n        <div class="content">\n            <p (click)="toggle()">{{ story.title }}</p>\n            <div class="extra">\n                <div class="ui label"><i class="star icon"></i>{{ story.effort }}</div>\n                <div class="ui label">Some useful tag</div>\n                <button class="ui right floated mini button">Start</button>\n            </div>\n        </div>\n    </div>\n    <div class="ui segment" *ngIf="open" story-detail [story]="story"></div>\n'},288:/*!****************************************************************************************!*\
-  !*** ./workmate/static/workmate/ng/components/story-list/story-list.component.html.ts ***!
-  \****************************************************************************************/
-function(t,e){"use strict";e.htmlTemplate='\n\n    <div class="ui segment">\n        <button class="ui mini compact right floated icon button" (click)="newStory()"><i class="plus icon"></i></button>\n        <p>{{ title }}</p>\n    </div>\n    <div class="ui segment" *ngFor="let story of stories" story-list-item [story]="story"></div>\n    \n'}});
+webpackJsonp([ 0 ], {
+    0: function(t, e, o) {
+        "use strict";
+        var n = this && this.__decorate || function(t, e, o, n) {
+            var i, r = arguments.length, s = 3 > r ? e : null === n ? n = Object.getOwnPropertyDescriptor(e, o) : n;
+            if ("object" == typeof Reflect && "function" == typeof Reflect.decorate) s = Reflect.decorate(t, e, o, n); else for (var c = t.length - 1; c >= 0; c--) (i = t[c]) && (s = (3 > r ? i(s) : r > 3 ? i(e, o, s) : i(e, o)) || s);
+            return r > 3 && s && Object.defineProperty(e, o, s), s;
+        }, i = this && this.__metadata || function(t, e) {
+            return "object" == typeof Reflect && "function" == typeof Reflect.metadata ? Reflect.metadata(t, e) : void 0;
+        }, r = o(1), s = o(97), c = o(280), a = function() {
+            function t() {}
+            return t = n([ r.Component({
+                selector: "agile-app",
+                template: '\n        <div class="ui equal width grid">\n            <div class="column">\n                <div class="ui raised segments" story-list [title]="\'Backlog\'"></div>\n            </div>\n            <div class="column">\n                <div class="ui raised segments" story-list [title]="\'Icebox\'"></div>\n            </div>\n        </div>\n    ',
+                directives: [ c.StoryListComponent ]
+            }), i("design:paramtypes", []) ], t);
+        }();
+        e.AgileComponent = a, s.bootstrap(a);
+    },
+    280: function(t, e, o) {
+        "use strict";
+        var n = this && this.__decorate || function(t, e, o, n) {
+            var i, r = arguments.length, s = 3 > r ? e : null === n ? n = Object.getOwnPropertyDescriptor(e, o) : n;
+            if ("object" == typeof Reflect && "function" == typeof Reflect.decorate) s = Reflect.decorate(t, e, o, n); else for (var c = t.length - 1; c >= 0; c--) (i = t[c]) && (s = (3 > r ? i(s) : r > 3 ? i(e, o, s) : i(e, o)) || s);
+            return r > 3 && s && Object.defineProperty(e, o, s), s;
+        }, i = this && this.__metadata || function(t, e) {
+            return "object" == typeof Reflect && "function" == typeof Reflect.metadata ? Reflect.metadata(t, e) : void 0;
+        }, r = o(1), s = o(281), c = o(283), a = o(288), l = function() {
+            function t(t) {
+                this.storyService = t, this.newStory = function() {
+                    this.stories.push({
+                        id: this.stories.length + 1,
+                        title: "New story"
+                    });
+                };
+            }
+            return t.prototype.getStories = function() {
+                var t = this;
+                this.storyService.getStories().then(function(e) {
+                    return t.stories = e;
+                });
+            }, t.prototype.ngOnInit = function() {
+                this.getStories();
+            }, n([ r.Input(), i("design:type", String) ], t.prototype, "title", void 0), t = n([ r.Component({
+                selector: "[story-list]",
+                template: a.htmlTemplate,
+                directives: [ c.StoryListItemComponent ],
+                providers: [ s.StoryService ]
+            }), i("design:paramtypes", [ s.StoryService ]) ], t);
+        }();
+        e.StoryListComponent = l;
+    },
+    281: function(t, e, o) {
+        "use strict";
+        var n = this && this.__decorate || function(t, e, o, n) {
+            var i, r = arguments.length, s = 3 > r ? e : null === n ? n = Object.getOwnPropertyDescriptor(e, o) : n;
+            if ("object" == typeof Reflect && "function" == typeof Reflect.decorate) s = Reflect.decorate(t, e, o, n); else for (var c = t.length - 1; c >= 0; c--) (i = t[c]) && (s = (3 > r ? i(s) : r > 3 ? i(e, o, s) : i(e, o)) || s);
+            return r > 3 && s && Object.defineProperty(e, o, s), s;
+        }, i = this && this.__metadata || function(t, e) {
+            return "object" == typeof Reflect && "function" == typeof Reflect.metadata ? Reflect.metadata(t, e) : void 0;
+        }, r = o(1), s = o(282), c = function() {
+            function t() {}
+            return t.prototype.getStories = function() {
+                return Promise.resolve(s.STORIES);
+            }, t = n([ r.Injectable(), i("design:paramtypes", []) ], t);
+        }();
+        e.StoryService = c;
+    },
+    282: function(t, e) {
+        "use strict";
+        e.STORIES = [ {
+            id: 1,
+            title: "I want to do foo",
+            effort: .5
+        }, {
+            id: 2,
+            title: "I want to do bar",
+            effort: 2
+        }, {
+            id: 3,
+            title: "I want to do wiz",
+            effort: 1
+        }, {
+            id: 4,
+            title: "I want to do bang",
+            effort: 1
+        }, {
+            id: 5,
+            title: "I want to do pop",
+            effort: 3
+        } ];
+    },
+    283: function(t, e, o) {
+        "use strict";
+        var n = this && this.__decorate || function(t, e, o, n) {
+            var i, r = arguments.length, s = 3 > r ? e : null === n ? n = Object.getOwnPropertyDescriptor(e, o) : n;
+            if ("object" == typeof Reflect && "function" == typeof Reflect.decorate) s = Reflect.decorate(t, e, o, n); else for (var c = t.length - 1; c >= 0; c--) (i = t[c]) && (s = (3 > r ? i(s) : r > 3 ? i(e, o, s) : i(e, o)) || s);
+            return r > 3 && s && Object.defineProperty(e, o, s), s;
+        }, i = this && this.__metadata || function(t, e) {
+            return "object" == typeof Reflect && "function" == typeof Reflect.metadata ? Reflect.metadata(t, e) : void 0;
+        }, r = o(1), s = o(284), c = o(285), a = o(287), l = function() {
+            function t() {
+                this.open = !1, this.toggle = function() {
+                    this.open = !this.open;
+                };
+            }
+            return n([ r.Input(), i("design:type", s.Story) ], t.prototype, "story", void 0), 
+            t = n([ r.Component({
+                selector: "[story-list-item]",
+                template: a.htmlTemplate,
+                directives: [ c.StoryDetailComponent ]
+            }), i("design:paramtypes", []) ], t);
+        }();
+        e.StoryListItemComponent = l;
+    },
+    284: function(t, e) {
+        "use strict";
+        var o = function() {
+            function t() {}
+            return t;
+        }();
+        e.Story = o;
+    },
+    285: function(t, e, o) {
+        "use strict";
+        var n = this && this.__decorate || function(t, e, o, n) {
+            var i, r = arguments.length, s = 3 > r ? e : null === n ? n = Object.getOwnPropertyDescriptor(e, o) : n;
+            if ("object" == typeof Reflect && "function" == typeof Reflect.decorate) s = Reflect.decorate(t, e, o, n); else for (var c = t.length - 1; c >= 0; c--) (i = t[c]) && (s = (3 > r ? i(s) : r > 3 ? i(e, o, s) : i(e, o)) || s);
+            return r > 3 && s && Object.defineProperty(e, o, s), s;
+        }, i = this && this.__metadata || function(t, e) {
+            return "object" == typeof Reflect && "function" == typeof Reflect.metadata ? Reflect.metadata(t, e) : void 0;
+        }, r = o(1), s = o(284), c = o(286), a = function() {
+            function t() {}
+            return n([ r.Input(), i("design:type", s.Story) ], t.prototype, "story", void 0), 
+            t = n([ r.Component({
+                selector: "[story-detail]",
+                template: c.htmlTemplate
+            }), i("design:paramtypes", []) ], t);
+        }();
+        e.StoryDetailComponent = a;
+    },
+    286: function(t, e) {
+        "use strict";
+        e.htmlTemplate = '\n    \n    <div class="ui label">ID<div class="detail">{{ story.id }}</div></div>\n    <div class="ui form">\n        <div class="field">\n            <textarea [(ngModel)]="story.title" rows="2" placeholder="title"></textarea>\n        </div>\n        <div class="field">\n            <select [(ngModel)]="story.effort" class="ui dropdown">\n                <option value="0.5">0.5 Points</option>\n                <option value="1">1 Point</option>\n                <option value="2">2 Points</option>\n                <option value="3">3 Points</option>\n                <option value="5">5 Points</option>\n            </select>\n        </div>\n    </div>\n    \n';
+    },
+    287: function(t, e) {
+        "use strict";
+        e.htmlTemplate = '\n\n    <div class="item">\n        <div class="content">\n            <p (click)="toggle()">{{ story.title }}</p>\n            <div class="extra">\n                <div class="ui label"><i class="star icon"></i>{{ story.effort }}</div>\n                <div class="ui label">Some useful tag</div>\n                <button class="ui right floated mini button">Start</button>\n            </div>\n        </div>\n    </div>\n    <div class="ui segment" *ngIf="open" story-detail [story]="story"></div>\n';
+    },
+    288: function(t, e) {
+        "use strict";
+        e.htmlTemplate = '\n\n    <div class="ui segment">\n        <button class="ui mini compact right floated icon button" (click)="newStory()"><i class="plus icon"></i></button>\n        <p>{{ title }}</p>\n    </div>\n    <div class="ui segment" *ngFor="let story of stories" story-list-item [story]="story"></div>\n    \n';
+    }
+});
