c192cad94c532e78c9ee47f51ae22e5c1ab4bc7b
Stuart George <stuart.bigmassa@gmail.com>
2016-06-04 10:27:32 +0100

    removed pointless template path

--- example/example/urls.py
+++ example/example/urls.py
@@ -15,7 +15,7 @@ from django.contrib.auth.views import (
 
 urlpatterns = [
 
-    url(r'^login/$', login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
     url(r'^password_change/$', password_change, name='password_change'),
     url(r'^password_change/done/$', password_change_done, name='password_change_done'),
